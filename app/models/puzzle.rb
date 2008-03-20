c17f5fa7ed4d4cae1f88ee68ba9c1264513e6117
Tim Habermaas <tim.habermaas@gmail.com>
2008-03-20 16:29:42 +0000

    rm index.html

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -1,5 +1,3 @@
 class Puzzle < ActiveRecord::Base
   belongs_to :kind
-  
-  file_column :image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :items
+
   map.root :controller => :puzzles
   
   map.resources :kinds
