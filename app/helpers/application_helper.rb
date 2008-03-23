1fd83879c0ee8775719bff4e7bbf7ce4adf2dcd7
Simon Wacker <simon.wacker@gmail.com>
2008-03-23 17:21:03 +0000

    added exception notofication plugin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,8 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def page_title
     params[:controller].titleize
   end
-
+  
   def action_label
     case params[:action].to_sym
       when :new, :create
@@ -14,6 +13,18 @@ module ApplicationHelper
   end
   
   def controller?(name)
-    params[:controller].to_sym == name
+    params[:controller].to_sym == name.to_sym
+  end
+  
+  def current_item?(item)
+    controller? item.url[:controller]
+  end
+  
+  def current_puzzle?(puzzle)
+    params[:puzzle] == puzzle.name
+  end
+  
+  def current_kind?(kind)
+    params[:kind] == kind.name
   end
 end
