5a6ad90bc95ed789712f4bfc1132944595098d55
Simon Wacker <simon.wacker@gmail.com>
2008-03-24 10:35:10 +0000

    made config rails 2.0

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,7 @@ config.cache_classes = true
 # Full error reports are disabled and caching is turned on
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
+config.action_view.cache_template_loading            = true
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
@@ -17,5 +18,8 @@ config.action_controller.perform_caching             = true
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
-ExceptionNotifier.exception_recipients = %w(tim.habermaas@gmail.com simon.wacker@gmail.com)
+# Disable delivery errors, bad email addresses will be ignored
+# config.action_mailer.raise_delivery_errors = false
+
+ExceptionNotifier.exception_recipients = %w(tim.habermaas+cubemania@gmail.com simon.wacker+cubemania@gmail.com)
 ExceptionNotifier.sender_address = %('Cubemania Error' <error@cubemania.org>)
