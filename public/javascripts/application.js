16a71971003c510aa58576fc37df4f8a84c42273
Simon Wacker <simon.wacker@gmail.com>
2008-03-25 17:33:09 +0000

    experimented with puzzles

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+/*function setPuzzlesWidth(id) {
+  var ul = $("#puzzles_" + id + " ul");
+  var li = ul.children("li");
+  var width = li.width() + parseFloat(li.css('margin-right'));
+  ul.width(++li.length * width);
+}*/
+
+function showPuzzles(index) {
+  $("#kinds").children("li").removeClass("checked").eq(index).addClass("checked");
+  var value = -$("#puzzles li").height() * index;
+  $("#puzzles ul").animate({top: value}, "normal");
+}
