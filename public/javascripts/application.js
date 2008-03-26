f2bbcf177caa30662fdbe5e5cd6e177c6ff1bce5
Simon Wacker <simon.wacker@gmail.com>
2008-03-26 18:44:27 +0000

    slide down subnavigation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,3 +2,7 @@ function checkKind(index) {
   $("#kinds").children("li").removeClass("checked").eq(index).addClass("checked");
   $("#puzzles ul").animate({left: -1000 * index}, "normal");
 }
+
+function showSubNavigation() {
+  $("#subnavigation").slideDown("normal");
+}
