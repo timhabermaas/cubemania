27ea2709ebbb961f1e46d3f8e6fb0e7036d4bf0f
Simon Wacker <simon.wacker@gmail.com>
2008-03-26 18:38:55 +0000

    fixed width; puzzles move horizontally

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,4 @@
-/*function setPuzzlesWidth(id) {
-  var ul = $("#puzzles_" + id + " ul");
-  var li = ul.children("li");
-  var width = li.width() + parseFloat(li.css('margin-right'));
-  ul.width(++li.length * width);
-}*/
-
 function checkKind(index) {
   $("#kinds").children("li").removeClass("checked").eq(index).addClass("checked");
-  var value = $("#puzzles li").height() * index;
-  $("#puzzles ul").animate({top: -value}, "normal");
+  $("#puzzles ul").animate({left: -1000 * index}, "normal");
 }
