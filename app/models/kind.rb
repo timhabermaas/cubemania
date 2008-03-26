3236247335824dce3846c2752938079f112780b1
Simon Wacker <simon.wacker@gmail.com>
2008-03-26 17:44:01 +0000

    kinds do not require an image

--- app/models/kind.rb
+++ app/models/kind.rb
@@ -3,7 +3,7 @@ class Kind < ActiveRecord::Base
   
   file_column :image, :store_dir => 'public/images/kinds', :base_url => 'images/kinds'
   
-  validates_presence_of :name, :image
+  validates_presence_of :name
   validates_length_of :name, :maximum => 64
   validates_filesize_of :image, :in => 0..10.kilobytes
   validates_file_format_of :image, :in => ['gif', 'png']
