cdbf9f1ae431b9a9e40e98c0a4c10ea8c6d81501
Tim Habermaas <tim.habermaas@gmail.com>
2008-04-17 17:54:56 +0000

    24 cubes for the win

--- app/controllers/clocks_controller.rb
+++ app/controllers/clocks_controller.rb
@@ -13,7 +13,6 @@ class ClocksController < ApplicationController
     if @average.save
       average = update_record user.averages.record(@average.puzzle_id), @average
       single = update_record user.singles.record(@average.puzzle_id), @average.singles.sort_by(&:time).first
-      # sorry simon, i've got no freaking idea what's wrong with my if else statements.
       if average
         flash[:notice] = "You have a new personal average record!"
       end
@@ -23,18 +22,15 @@ class ClocksController < ApplicationController
       if average && single
         flash[:notice] = "You have a new personal single and average record!"
       end
-      # What about a "You are ranked 21. with this average" message?
       respond_to { |format| format.js { @puzzle = @average.puzzle } }
     end
   end
 
   private
     def update_record(old, new)
-      updated = false
-      if (old.nil? or new.time < old.time) and not new.dnf?
+      if updated = (old.nil? or new.time < old.time) and not new.dnf?
         old.update_attribute :record, false unless old.nil?
         new.update_attribute :record, true
-        updated = true
       end
       updated
     end
