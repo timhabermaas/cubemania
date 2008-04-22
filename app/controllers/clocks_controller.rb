b8e9bc12a90648e985ded81e9e25574f807929ea
Tim Habermaas <tim.habermaas@gmail.com>
2008-04-22 19:59:01 +0000

    added transaction for recording average + record. i didn't use the after_save approach, since it could lead to inconsistent data. fixed a timer issue with worst/best + dnf

--- app/controllers/clocks_controller.rb
+++ app/controllers/clocks_controller.rb
@@ -6,17 +6,19 @@ class ClocksController < ApplicationController
   end
   
   def create
+    params[:average][:puzzle_id] = params[:puzzle_id]
     @average = current_user.averages.build params[:average]
-    @average.singles = params[:singles].map { |index, single| current_user.singles.build single }
+    @average.singles = params[:singles].map { |index, single| single[:puzzle_id] = params[:puzzle_id]; current_user.singles.build single }
 
-    if @average.save
-      #records should be updated within the same transaction, i.e. in after_save callback in model Average
-      average = update_record current_user.averages.record(@average.puzzle_id), @average
-      single = update_record current_user.singles.record(@average.puzzle_id), @average.singles.sort_by(&:time).first
-      flash[:notice] = 'You have a new personal average record!' if average
-      flash[:notice] = 'You have a new personal single record!' if single
-      flash[:notice] = 'You have a new personal single and average record!' if average and single
-      @puzzle = @average.puzzle
+    Average.transaction do
+      if @average.save
+        average = update_record current_user.averages.record(@average.puzzle_id), @average
+        single = update_record current_user.singles.record(@average.puzzle_id), @average.singles.sort_by(&:time).first
+        flash[:notice] = 'You have a new personal average record!' if average
+        flash[:notice] = 'You have a new personal single record!' if single
+        flash[:notice] = 'You have a new personal single and average record!' if average and single
+        @puzzle = @average.puzzle
+      end
     end
   end
 
