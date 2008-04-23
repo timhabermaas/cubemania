bc626364d7f0fc96e0612296f672c4dd7e351dd7
Tim Habermaas <tim.habermaas@gmail.com>
2008-04-23 00:15:10 +0000

    moved records updating to Average#before_save

--- app/controllers/clocks_controller.rb
+++ app/controllers/clocks_controller.rb
@@ -12,22 +12,11 @@ class ClocksController < ApplicationController
 
     Average.transaction do
       if @average.save
-        average = update_record current_user.averages.record(@average.puzzle_id), @average
-        single = update_record current_user.singles.record(@average.puzzle_id), @average.singles.sort_by(&:time).first
-        flash[:notice] = 'You have a new personal average record!' if average
-        flash[:notice] = 'You have a new personal single record!' if single
-        flash[:notice] = 'You have a new personal single and average record!' if average and single
+        flash[:notice] = 'You have a new personal average record!' if @average.record
+        flash[:notice] = 'You have a new personal single record!' unless @average.singles.select(&:record).empty?
+        flash[:notice] = 'You have a new personal single and average record!' if @average.record and not @average.singles.select(&:record).empty?
         @puzzle = @average.puzzle
       end
     end
   end
-
-  private
-    def update_record(old, new)
-      if updated = ((old.nil? or new.time < old.time) and not new.dnf?)
-        old.update_attribute :record, false unless old.nil?
-        new.update_attribute :record, true
-      end
-      updated
-    end
 end
--- app/models/average.rb
+++ app/models/average.rb
@@ -1,4 +1,22 @@
 class Average < Clock
   belongs_to :user, :counter_cache => true; attr_protected :user_id, :user
   has_many :singles, :order => 'time', :dependent => :destroy
+  
+  before_save :update_records
+  
+private
+  def update_records
+    current_user = User.find self.user_id
+    average_record = current_user.averages.record(self.puzzle_id)
+    single_record = current_user.singles.record(self.puzzle_id)
+    update_record average_record, self
+    update_record single_record, self.singles.first
+  end
+
+  def update_record(old, new)
+    if (old.nil? or new.time < old.time) and not new.dnf?
+      old.update_attribute :record, false unless old.nil?
+      new.record = true
+    end
+  end
 end
