807f49b7790fcb9880ce021dc8f787da2c5e6aa1
Tim Habermaas <tim.habermaas@gmail.com>
2008-04-23 21:57:24 +0000

    blub

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,11 +14,9 @@ namespace :deploy do
   end
 
   task :restart, :roles => :app do
-    invoke_command "cd #{application_path}"
-    invoke_command "svn up"
-    invoke_command "RAILS_ENV=production rake db:migrate"
-    invoke_command "rm #{application_path}/public/javascripts/all.js"
-    invoke_command "rm #{application_path}/public/stylesheets/all.css"
+    invoke_command "cd #{application_path}; svn up"
+    invoke_command "cd #{application_path}; rake db:migrate"
+    invoke_command "rm #{application_path}/public/javascripts/all.js #{application_path}/public/stylesheets/all.css"
     invoke_command "/usr/local/bin/mongrel_rails restart -P #{home}/var/run/mongrel-#{application}-#{mongrel_port}.pid"
   end
 
