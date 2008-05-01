3ee65483b43b0290aa682a60f147b9f0b6d17a11
Simon Wacker <simon.wacker@gmail.com>
2008-05-01 16:32:50 +0000

    comments user_id and post_d is nullified when user gets deleted; we wouldn't be able to update the comment if user_id is required

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :post, :counter_cache => true
   belongs_to :user; attr_protected :user_id, :user
 
-  validates_presence_of :content, :post_id, :user_id
+  validates_presence_of :content, :post_id
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :user; attr_protected :user_id, :user
   has_many :comments, :include => :user, :order => 'comments.created_at', :dependent => :destroy
 
-  validates_presence_of :title, :content, :user_id
+  validates_presence_of :title, :content
   validates_length_of :title, :maximum => 64
 end
