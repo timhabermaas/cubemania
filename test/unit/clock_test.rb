10904b86f4637314bb6f3360ca0c212051ae0b40
Tim Habermaas <tim.habermaas@gmail.com>
2008-05-06 16:04:07 +0000

    Added trac link to Home

--- test/unit/clock_test.rb
+++ test/unit/clock_test.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ClockTest < ActiveSupport::TestCase
-  fixtures :clocks, :users, :kinds, :puzzles
+  fixtures :all
   def test_truth
     assert puzzles('3x3x3').scramble_length == 25
   end
