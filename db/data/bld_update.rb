3aa1088a6fb01b7547da0f3aab68abd4b17d6367
Tim Habermaas <tim.habermaas@gmail.com>
2008-05-09 00:23:45 +0000

    fuck...

--- db/data/bld_update.rb
+++ db/data/bld_update.rb
@@ -1,5 +1,6 @@
 Average.transaction do
   kind = Kind.find_by_name 'blindfolded'
+  users = User.find :all
   kind.puzzles.each do |p|
     averages = Average.find_all_by_puzzle_id p.id
     averages.each do |a|
@@ -7,6 +8,8 @@ Average.transaction do
       a.update_attribute :dnf, a.singles.reject(&:dnf).empty?
       a.update_attribute :record, false
     end
-    averages.reject(&:dnf).sort_by(&:time).first.update_attribute :record, true unless averages.reject(&:dnf).empty?
+    users.each do |u|
+      u.averages.for(p.id).sort_by(&:time).reject(&:dnf).first.update_attribute :record, true unless u.averages.for(p.id).reject(&:dnf).empty?
+    end
   end
 end
