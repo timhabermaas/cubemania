a44a7b78cd33f957394b9e35d4966195b9a1e956
Tim Habermaas <tim.habermaas@gmail.com>
2008-05-12 17:28:13 +0000

    Redirecting killed my times ;)

--- app/controllers/averages_controller.rb
+++ app/controllers/averages_controller.rb
@@ -11,10 +11,6 @@ class AveragesController < ApplicationController
       format.xml { @averages = @user.averages.for params[:puzzle_id] }
       format.csv do
         @averages = @user.averages.for params[:puzzle_id], true
-        if @averages.empty?
-          flash[:notice] = 'You have no times yet. Use the timer!'
-          redirect_to kind_puzzle_times_path
-        end
         response.headers['Content-Type'] = 'text/csv; charset=iso-8859-1; header=present'
         response.headers['Content-Disposition'] = "attachment; filename=#{@puzzle.kind.name}_#{@puzzle.name}.csv"
       end
