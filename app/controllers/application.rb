461de9dbaaf1c6e62f5687f4ef6b1080bffe76b6
Simon Wacker <simon.wacker@gmail.com>
2008-05-15 20:32:55 +0000

    set session expiration date to one month for all users instead of adding a remember me check box. i personally don't like remember me buttons and because cubemania doesn't have to be overly secure saving the session for one month is okay. people may of course log out if they fear others could use their account.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,13 +2,22 @@ class ApplicationController < ActionController::Base
   include Authentication
   include ExceptionNotifiable
 
+  before_filter :update_session_expiration_date
+
   login :except => [:index, :show]
 
   protect_from_forgery
 
   filter_parameter_logging 'password'
-  
+
   def rescue_action_in_public(exception)
     render :template => "errors/#{response_code_for_rescue(exception)}"
   end
+
+  private
+    def update_session_expiration_date
+      unless ActionController::Base.session_options[:session_expires]
+        ActionController::Base.session_options[:session_expires] = 1.month.from_now
+      end
+    end
 end
