66d812f7fffab2b03512bde54c599ecbda2b73b8
Simon Wacker <simon.wacker@gmail.com>
2008-05-23 16:40:28 +0000

    oh well

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,7 +2,12 @@ class Competition < ActiveRecord::Base
   REPEATS = %w{once daily weekly monthly}
 
   belongs_to :puzzle
-  has_many :averages, :include => :user, :order => 'time', :dependent => :nullify
+  has_many :averages, :include => :user, :order => 'time', :dependent => :nullify do
+    def current(competition); self.for competition; end
+    def for(competition, time = Time.now)
+      find :all, :conditions => ['created_at >= ? and created_at < ?', competition.started_at(time), competition.ended_at(time)]
+    end
+  end
   has_many :singles, :dependent => :nullify
   belongs_to :user; attr_protected :user_id, :user
 
@@ -12,23 +17,23 @@ class Competition < ActiveRecord::Base
     averages.collect { |a| a.user }.include? user
   end
 
-  def started_at
+  def started_at(time = Time.now)
     if repeat == 'once'
       created_at
     else
-      Time.now.send "beginning_of_#{nominalize_repeat}"
+      time.send "beginning_of_#{nominalize_repeat}"
     end
   end
 
-  def ends_at
+  def ended_at(time = Time.now)
     if repeat == 'once'
       created_at.next_month
     else
-      Time.now.send "end_of_#{nominalize_repeat}"
+      time.send "end_of_#{nominalize_repeat}"
     end
   end
 
-  private
+  private    
     def nominalize_repeat
       repeat == 'daily' ? 'day' : repeat[0..-3]
     end
