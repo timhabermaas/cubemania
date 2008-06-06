e29219114631bfe0701b9605e55e7af77c939d8d
Simon Wacker <simon.wacker@gmail.com>
2008-06-06 08:55:00 +0000

    the database isn't accessible from outside (makes deployment easier). use gmail to send emails

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, 'cubemania.org'
-set :user, 'simon'
-set :svn_user, 'simonwacker'
-set :svn_password, Proc.new { Capistrano::CLI.password_prompt('SVN Password: ') }
+set :user, Proc.new { Capistrano::CLI.ui.ask 'User: ' }
+set :svn_user, Proc.new { Capistrano::CLI.ui.ask 'SVN User: ' }
+set :svn_password, Proc.new { Capistrano::CLI.password_prompt 'SVN Password: ' }
 set :repository, Proc.new { "--username #{svn_user} --password #{svn_password} --no-auth-cache http://svn2.assembla.com/svn/cubemania/trunk" }
 set :deploy_to, "/srv/#{application}"
 set :ip, '209.20.70.85'
