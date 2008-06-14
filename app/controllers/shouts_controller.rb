dca90c20ec5863952214321dee1236a38201692d
Simon Wacker <simon.wacker@gmail.com>
2008-06-14 15:52:54 +0000

    google ads aren't pretty indeed.

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -1,19 +1,19 @@
 class ShoutsController < ApplicationController
   permit :moderator, :only => :destroy
-  
+
   def create
     shout = current_user.shouts.build params[:shout].merge :competition_id => params[:competition_id]
     shout.save
     redirect_to puzzle_competition_path(params[:puzzle_id], params[:competition_id])
   end
-  
+
   def destroy
     object.destroy
     redirect_to puzzle_competition_path(params[:puzzle_id], params[:competition_id])
   end
-  
-private
-  def object
-    @shout ||= Shout.find params[:id]
-  end
+
+  private
+    def object
+      @shout ||= Shout.find params[:id]
+    end
 end
