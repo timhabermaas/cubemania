70b50ee08a0c1cbe47bf6eb24827a395d523f709
Simon Wacker <simon.wacker@gmail.com>
2008-06-20 17:25:31 +0000

    sorry

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -4,13 +4,13 @@ class ShoutsController < ApplicationController
   def create
     @puzzle = Puzzle.find params[:puzzle_id]
     @competition = @puzzle.competitions.find params[:competition_id]
-    @shout = current_user.shouts.build params[:shout].merge :competition_id => @competition
+    @shout = current_user.shouts.build params[:shout].merge :competition_id => @competition.id
     @shout.save
   end
 
   def destroy
     object.destroy
-    redirect_to puzzle_competition_path params[:puzzle_id], params[:competition_id], :anchor => 'comments'
+    redirect_to puzzle_competition_path params[:puzzle_id], params[:competition_id], :anchor => 'shouts'
   end
 
   private
