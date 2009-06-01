b5e18eb6365afae2aa7460147f9550dfa8d72a72
Tim Habermaas <tim.habermaas@gmail.com>
2009-06-01 20:51:43 +0000

    removed pagination

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
   end
   has_many :averages, :order => 'created_at desc', :dependent => :delete_all do
     def for(puzzle_id); find_all_by_puzzle_id puzzle_id, :include => :singles; end
-    def paginate_for(puzzle_id, page); paginate :per_page => 100, :page => page, :conditions => { :puzzle_id => puzzle_id}, :include => :singles; end
+    def paginate_for(puzzle_id, page); find_all_by_puzzle_id puzzle_id, :include => :singles, :limit => 100; end
     def record(puzzle_id); find_by_puzzle_id_and_record puzzle_id, true; end
     def records; find_all_by_record true, :include => { :puzzle => :kind }, :order => 'puzzles.name, kinds.name'; end
     def best(puzzle_id); find_by_puzzle_id puzzle_id, :conditions => ['dnf = ?', false], :order => 'time'; end
