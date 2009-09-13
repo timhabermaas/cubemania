ea9f9feef3add9f54d186e9b419a0104f269a849
Tim Habermaas <tim.habermaas@gmail.com>
2009-09-13 12:54:12 +0000

    made shouts safe

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,6 +1,6 @@
 class Shout < ActiveRecord::Base
   belongs_to :competition
-  belongs_to :user
+  belongs_to :user, attr_protected :user_id, :user
   
   validates_presence_of :content, :user_id, :competition_id
 end
