61a8b56065a00d06aadc4258cf3a49dba00a0b97
Tim Habermaas <tim.habermaas@gmail.com>
2009-10-03 15:10:46 +0000

    added seed data

---
+++ db/seed.rb
@@ -0,0 +1,11 @@
+Item.create!(:name => 'Home', :description => 'Home', :url => '/', :position => 0)
+Item.create!(:name => 'Timer', :description => 'Timer', :url => '/puzzles/4/times', :position => 1)
+Item.create!(:name => 'Competitions', :description => 'Competitions', :url => '/puzzles/4/competitions', :position => 2)
+Item.create!(:name => 'Users', :description => 'Users', :url => '/users', :position => 3)
+Item.create!(:name => 'Records', :description => 'Records', :url => '/puzzles/4/times', :position => 4)
+
+k = Kind.create!(:name => 'speed')
+Kind.create!(:name => 'blindfolded')
+Kind.create!(:name => 'one-handed')
+
+Puzzle.create!(:name => '3x3x3', :kind => k, :scramble_length => 25, :attempt_count => 5, :average_format => 'average')
