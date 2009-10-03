54cfc29e718d168c9595740a3c0cba80e0f7b7ee
Tim Habermaas <tim.habermaas@gmail.com>
2009-10-03 15:13:13 +0000

    fixed id for seed data

--- db/seed.rb
+++ db/seed.rb
@@ -1,8 +1,8 @@
 Item.create!(:name => 'Home', :description => 'Home', :url => '/', :position => 0)
-Item.create!(:name => 'Timer', :description => 'Timer', :url => '/puzzles/4/times', :position => 1)
-Item.create!(:name => 'Competitions', :description => 'Competitions', :url => '/puzzles/4/competitions', :position => 2)
+Item.create!(:name => 'Timer', :description => 'Timer', :url => '/puzzles/1/times', :position => 1)
+Item.create!(:name => 'Competitions', :description => 'Competitions', :url => '/puzzles/1/competitions', :position => 2)
 Item.create!(:name => 'Users', :description => 'Users', :url => '/users', :position => 3)
-Item.create!(:name => 'Records', :description => 'Records', :url => '/puzzles/4/times', :position => 4)
+Item.create!(:name => 'Records', :description => 'Records', :url => '/puzzles/1/times', :position => 4)
 
 k = Kind.create!(:name => 'speed')
 Kind.create!(:name => 'blindfolded')
