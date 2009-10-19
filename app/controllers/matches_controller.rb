9b8f194e868be7a716218f9a3854273491364c6b
Tim Habermaas <tim.habermaas@gmail.com>
2009-10-19 21:50:35 +0000

    basic matches controller

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,15 +1,21 @@
 class MatchesController < ApplicationController
   
   def index
+    #@requests = current_user.guest_matches.unfinished
+    @matches = current_user.matches#current_user.home_matches.finished + current_user.guest_matches.finished
   end
 
   def show
-    @match = Match.find params[:id]
+    @puzzle = Puzzle.find params[:puzzle_id]
+    @match = @puzzle.matches.find params[:id]
+    @scrambles = @puzzle.scrambles
   end
 
   def create
-    @match = current_user.matches.build(:puzzle_id => params[:puzzle_id], :opponent_id => params[:user_id])
+    @user = User.find params[:user_id]
+    @match = current_user.home_matches.build(:puzzle_id => params[:puzzle_id], :opponent_id => params[:user_id])
     @match.save
+    redirect_to puzzle_match_path(params[:puzzle_id], @match)
   end
 
   def new
