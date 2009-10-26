2bed46712f2efe8c791666702644940e67229232
Tim Habermaas <tim.habermaas@gmail.com>
2009-10-26 22:36:22 +0000

    refactored clock model

--- app/models/clock.rb
+++ app/models/clock.rb
@@ -11,14 +11,10 @@ class Clock < ActiveRecord::Base
   validates_length_of :scramble, :maximum => 1024, :allow_nil => true
   
   def <=>(other)
-    if dnf and other.dnf?
-      0
-    elsif dnf
-      1
-    elsif other.dnf?
-      -1
+    if dnf?
+      other.dnf? ? 0 : 1
     else
-      time <=> other.time
+      other.dnf? ? -1 : (time <=> other.time)
     end
   end
 end
