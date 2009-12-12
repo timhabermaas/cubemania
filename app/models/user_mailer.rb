f0d8f7bcdcda942c806218f7d490b771b994b1c3
Tim Habermaas <tim.habermaas@gmail.com>
2009-12-12 02:17:40 +0000

    argh

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ActionMailer::Base
   def welcome_mail(user)
     recipients  user.email
-    from        'Cubemania <info@cubemania.org>'
-    subject "Welcome to Cubemania"
+    from 'Cubemania <info@cubemania.org>'
+    subject 'Welcome to Cubemania'
     sent_on Time.now
     body :user => user
   end
@@ -10,7 +10,7 @@ class UserMailer < ActionMailer::Base
   def password_reset_mail(user, password)
     recipients user.email
     from 'Cubemania <password@cubemania.org>'
-    subject "New Password for Cubemania"
+    subject 'New Password for Cubemania'
     sent_on Time.now
     body :password => password, :user => user
   end
