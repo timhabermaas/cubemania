94450e193b1274f60ab8740939efbbd81861de15
Tim Habermaas <tim.habermaas@gmail.com>
2010-04-01 22:53:51 +0200

    removed stupid touching of users

--- db/migrate/20091209164814_add_updated_at_to_user.rb
+++ db/migrate/20091209164814_add_updated_at_to_user.rb
@@ -1,9 +1,6 @@
 class AddUpdatedAtToUser < ActiveRecord::Migration
   def self.up
     add_column :users, :updated_at, :datetime
-    User.all.each do |user|
-      user.touch
-    end
   end
 
   def self.down
