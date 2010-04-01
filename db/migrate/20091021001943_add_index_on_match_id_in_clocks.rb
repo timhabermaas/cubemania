d0202f03770faf1735db6fdf19be3203a97e4fb8
Tim Habermaas <tim.habermaas@gmail.com>
2010-04-02 01:04:21 +0200

    increased timeout for index creation on clocks

--- db/migrate/20091021001943_add_index_on_match_id_in_clocks.rb
+++ db/migrate/20091021001943_add_index_on_match_id_in_clocks.rb
@@ -1,5 +1,7 @@
 class AddIndexOnMatchIdInClocks < ActiveRecord::Migration
   def self.up
+    ActiveRecord::Base.connection.select_all('set statement_timeout to
+    600000')
     add_index :clocks, [:match_id, :user_id], :name => "index_clocks_on_match_id_and_user_id"
   end
 
