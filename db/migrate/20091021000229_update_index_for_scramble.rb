c03447430956273aa87e978ea9e3ec58f6254125
Tim Habermaas <tim.habermaas@gmail.com>
2010-04-02 00:58:40 +0200

    fixed migration

--- db/migrate/20091021000229_update_index_for_scramble.rb
+++ db/migrate/20091021000229_update_index_for_scramble.rb
@@ -1,11 +1,11 @@
 class UpdateIndexForScramble < ActiveRecord::Migration
   def self.up
-    remove_index :scrambles, :name => "index_scrambles_on_competition_id_and_created_at_and_position"
+    remove_index :scrambles, [:competition_id, :created_at, :position]
     add_index :scrambles, [:matchable_id, :matchable_type, :created_at, :position], :name => "index_scrambles_on_matchable_and_created_at_and_position"
   end
 
   def self.down
     remove_index :scrambles, :name => "index_scrambles_on_matchable_and_created_at_and_position"
-    add_index :scrambles, [:created_at, :position], :name => "index_scrambles_on_competition_id_and_created_at_and_position"
+    add_index :scrambles, [:competition_id, :created_at, :position], :name => "index_scrambles_on_competition_id_and_created_at_and_position"
   end
 end
