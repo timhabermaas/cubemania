ba1b19767b82fefc67d60866a8efb3c5f5c98184
Tim Habermaas <tim.habermaas@gmail.com>
2010-04-06 23:13:29 +0200

    used ActiveModel to simplify Login model

--- app/models/login.rb
+++ app/models/login.rb
@@ -1,44 +1,22 @@
-require 'active_record/validations'
+#require 'active_record/validations'
 
 class Login
+  include ActiveModel::Naming
+  include ActiveModel::Validations
+  
   attr_accessor :name, :password
-
-  def initialize(attributes = nil)
-    unless attributes.nil?
-      self.name = attributes[:name]
-      self.password = attributes[:password]
-    end
-    @errors = ActiveRecord::Errors.new self
-  end
-
+  
   def validate
     user = User.find_by_name_and_password name, password
     errors.add :password, 'does not match name' if user.nil?
     self.password = nil
     user
   end
-
-  def id
-    nil
-  end
-
-  def errors
-    @errors
-  end
   
-  def new_record?
-    true
-  end
-  
-  def self.human_attribute_name(attr)
-    attr.to_s.titleize
-  end
-  
-  def self.human_name
-    'Login'
-  end
-  
-  def self.self_and_descendants_from_active_record
-    [self]
+  def initialize(attributes = nil)
+    if attributes
+      @name = attributes[:name]
+      @password = attributes[:password]
+    end
   end
 end
