49d790a36f69a32f73a20be4f8004974c1b37198
Tim Habermaas <tim.habermaas@gmail.com>
2010-04-08 00:32:18 +0200

    en-resource_controllified items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,43 @@
-class ItemsController < ResourceController::Base
+class ItemsController < ApplicationController
   permit :admin
-
-  [update, create].each do |action|
-    action.wants.html { redirect_to items_path }
+  
+  def index
+    @items = Item.all
   end
-
-  private
-    def collection
-      @collection ||= Item.all
+  
+  def new
+    @items = Item.new
+  end
+  
+  def edit
+    @item = Item.find params[:id]
+  end
+  
+  def destroy
+    @item = Item.find params[:id]
+    @item.destroy
+    redirect_to items_path
+  end
+  
+  def update
+    @item = Item.find params[:id]
+    if @item.update_attributes param[:item]
+      redirect_to items_path
+    else
+      render :edit
     end
+  end
+  
+  def create
+    @item = Item.new params[:item]
+    if @item.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+  
+  def object
+    Item.find params[:id]
+  end
 end
