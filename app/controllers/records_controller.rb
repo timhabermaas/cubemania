d4b7c82404c68a9fea3407bc3fc95e6a62b49ace
Tim Habermaas <tim.habermaas@gmail.com>
2010-04-09 00:29:54 +0200

    fixed caching

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,6 +1,10 @@
 class RecordsController < ApplicationController
   def index
     @puzzle = Puzzle.find params[:puzzle_id]
-    @records = @puzzle.records.send params[:type], params[:page]
+    @records = Clock.where(:record => true).
+                     where(:type => params[:type].capitalize).
+                     where(:puzzle_id => @puzzle.id).
+                     joins(:user).where('users.ignored' => false).order(:time)
+    #@records = @puzzle.records.send params[:type], params[:page]
   end
 end
