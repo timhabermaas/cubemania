714b625ee350293197185e465ad99e12b7d3b388
Tim Habermaas <tim.habermaas@gmail.com>
2010-04-08 20:30:03 +0200

    KindsController is also free

--- app/controllers/kinds_controller.rb
+++ app/controllers/kinds_controller.rb
@@ -1,7 +1,37 @@
-class KindsController < ResourceController::Base
+class KindsController < ApplicationController
   permit :admin
   
-  [update, create].each do |action|
-    action.wants.html { redirect_to kinds_path }
+  def index
+    @kinds = Kind.all
+  end
+  
+  def new
+    @kind = Kind.new
+  end
+  
+  def edit
+    @kind = Kind.find params[:id]
+  end
+  
+  def create
+    @kind = Kind.new params[:kind]
+    if @kind.save
+      redirect_to kinds_path
+    else
+      render :new
+    end
+  end
+  
+  def update
+    @kind = Kind.find params[:id]
+    if @kind.update_attributes params[:kind]
+      redirect_to kinds_path
+    else
+      render :edit
+    end
+  end
+  
+  def object
+    Kind.find params[:id]
   end
 end
