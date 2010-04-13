f62173aef6d73810be89d5653384a1490a5236ae
Tim Habermaas <tim.habermaas@gmail.com>
2010-04-14 01:04:50 +0200

    removed ugly timeout hack

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Cubemania::Application.initialize!
-
-# set timeout for database queries to 20s
-ActiveRecord::Base.connection.select_all('set statement_timeout to 20000') if Rails.env == 'production'
