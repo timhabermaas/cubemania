0d7a73c09051153723c2d7f628c3f1b57c1cef5f
Tim Habermaas <tim.habermaas@gmail.com>
2010-04-15 20:34:10 +0200

    added default encoding for templates

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,9 @@ module Cubemania
     #   g.test_framework  :test_unit, :fixture => true
     # end
 
+    # Configure the default encoding used in templates for Ruby 1.9.
+    config.encoding = "utf-8"
+
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters << :password << :password_confirmation
   end
