0dd8c23b7ad467c25f1baecc205e32c0ea4f3c5f
Tim Habermaas <tim.habermaas@gmail.com>
2010-04-18 16:06:08 +0200

    added mail config

---
+++ config/initializers/setup_email.rb
@@ -0,0 +1,12 @@
+ActionMailer::Base.smtp_settings = {
+  :enable_starttls_auto => true,
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => "cubemania.org",
+  :user_name => "info",
+  :password => ENV['EMAIL_PASSWORD'] || "blub",
+  :authentication => :plain
+}
+
+ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
