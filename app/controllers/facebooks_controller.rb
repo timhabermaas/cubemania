1fa2541d9a94a20122e6cc54450109ee9ac7d001
Tim Habermaas <tim.habermaas@gmail.com>
2010-04-22 15:36:27 +0200

    added notice after successfully connecting to facebook

--- app/controllers/facebooks_controller.rb
+++ app/controllers/facebooks_controller.rb
@@ -8,6 +8,7 @@ class FacebooksController < ApplicationController
   def callback
     access_token = facebook_consumer.authorize!(params[:code])
     current_user.update_attribute :fb_access_token, access_token
+    flash[:notice] = "You are now connected to Facebook."
     redirect_back root_path
   end
 end
