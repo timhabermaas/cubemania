bc269ae82a4fa924fda3c7167583d42cb273b16d
Tim Habermaas <tim.habermaas@gmail.com>
2010-04-22 23:27:31 +0200

    disable caching in development mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,8 +12,8 @@ Cubemania::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_view.debug_rjs             = true
-  config.action_controller.perform_caching = true
-  config.cache_store = :mem_cache_store, Memcached::Rails.new
+  config.action_controller.perform_caching = false
+  #config.cache_store = :mem_cache_store, Memcached::Rails.new
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
