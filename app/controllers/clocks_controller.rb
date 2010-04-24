02c355f907cf73c4532a173ec7d90daf92f64c41
Tim Habermaas <tim.habermaas@gmail.com>
2010-04-24 13:37:30 +0200

    formatting

--- app/controllers/clocks_controller.rb
+++ app/controllers/clocks_controller.rb
@@ -4,13 +4,16 @@ class ClocksController < ApplicationController
   def index
     @puzzle = Puzzle.find params[:puzzle_id]
     @scrambles = @puzzle.scrambles
-    store_location
   end
 
   def create
     @average = current_user.averages.build params[:average].merge(:puzzle_id => params[:puzzle_id],
-                :competition_id => params[:competition_id], :match_id => params[:match_id])
-    @average.singles = params[:singles].map { |index, single| current_user.singles.build single.merge(:puzzle_id => params[:puzzle_id], :competition_id => params[:competition_id], :match_id => params[:match_id], :position => index) }
+                                                                  :competition_id => params[:competition_id],
+                                                                  :match_id => params[:match_id])
+    @average.singles = params[:singles].map { |index, single| current_user.singles.build single.merge(:puzzle_id => params[:puzzle_id],
+                                                                                                      :competition_id => params[:competition_id],
+                                                                                                      :match_id => params[:match_id],
+                                                                                                      :position => index) }
 
     if @average.save
       @puzzle = @average.puzzle
