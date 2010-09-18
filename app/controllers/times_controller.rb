5d74e89634958a1bffbe6e2027aae1a93eb6eaa9
Tim Habermaas <tim.habermaas@gmail.com>
2010-09-18 21:41:12 +0200

    added basic timer functionality

--- app/controllers/times_controller.rb
+++ app/controllers/times_controller.rb
@@ -9,17 +9,13 @@ class TimesController < ApplicationController
   end
 
   def create
-    @average = current_user.averages.build params[:average].merge(:puzzle_id => params[:puzzle_id],
-                                                                  :competition_id => params[:competition_id],
-                                                                  :match_id => params[:match_id])
-    @average.singles = params[:singles].map { |index, single| current_user.singles.build single.merge(:puzzle_id => params[:puzzle_id],
-                                                                                                      :competition_id => params[:competition_id],
-                                                                                                      :match_id => params[:match_id],
-                                                                                                      :position => index) }
-
-    if @average.save
-      @puzzle = @average.puzzle
-      @scrambles = @puzzle.scrambles
+    @puzzle = Puzzle.find params[:puzzle_id]
+    @single = current_user.singles.build(params[:single].merge!(:puzzle_id => @puzzle.id))
+    if @single.save
+      respond_to do |format|
+        format.html { redirect_to puzzle_times_path(@puzzle) }
+        format.js
+      end
     end
   end
 
--- app/models/single.rb
+++ app/models/single.rb
@@ -2,12 +2,38 @@ class Single < ActiveRecord::Base
   belongs_to :user, :counter_cache => true
   belongs_to :puzzle
 
-  validates_presence_of :user_id, :puzzle_id, :time
+  attr_accessible :time, :human_time, :puzzle_id, :scramble
 
+  validates_presence_of :user_id, :puzzle_id, :time
+  validates_format_of :human_time, :with => /\A(\d+:)?(\d+:)?\d+(.\d+)?\s*(min|s|h)?\Z/
+
+  before_validation :set_time
   after_save :check_for_new_record
+
+  def human_time(spacer = '')
+    t = self.time || 0
+    hs = (t / 10.0).round
+    if hs >= 6000
+      min = hs / 6000
+      sec = (hs - min * 6000) / 100.0
+      '%d:%05.2f' % [min, sec] + spacer + 'min' # 12.555 => "12.55"
+    else
+      '%.2f' % (hs.to_f / 100) + spacer + 's'
+    end
+  end
+
+  # TODO check for correct format. option 1: validates_format_of + before_save, option 2: don't know
+  def human_time=(ht)
+    @human_time = ht
+  end
 
 private
   def check_for_new_record
-    # get the last 5 solves, calculate an average. if the average is lower than user.average_records.for(puzzle_id).time
+    # get the last 5 solves, calculate an average. if the average is lower than user.average_records.for(puzzle_id).time replace the old one
+  end
+
+  def set_time
+    seconds, minutes, hours = @human_time.split(':').reverse
+    self.time = (hours.to_i * 3600 + minutes.to_i * 60) * 1000 + (seconds.to_f * 1000).to_i
   end
 end
