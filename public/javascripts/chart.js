3fc07bda90311404bf116e5e1700cd6f6cfa864c
Tim Habermaas <tim.habermaas@gmail.com>
2010-09-19 01:32:57 +0200

    adding chart.

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -1,5 +1,40 @@
+var chart;
+
 $(document).ready(function() {
-  var singles = $("#timer #singles li time").map(function() {
+  var singles = $("#singles li time").map(function() {
     return $(this).attr("data-time");
   }).get();
+
+  chart = new Highcharts.Chart({
+           chart: {
+             renderTo: 'chart'
+           },
+           title: {
+             text: 'Cubing Progress'
+           },
+           xAxis: {
+           },
+           yAxis: {
+              title: {
+                 text: 'Time'
+              },
+              labels: {
+                formatter: function() {
+                  return formatTime(this.value);
+                }
+              }
+           },
+           plotOptions: {
+                    line: {
+                       dataLabels: {
+                          enabled: true
+                       },
+                       enableMouseTracking: false
+                    }
+                 },
+           series: [{
+              name: 'tim',
+              data: singles
+           }]
+        });
 });
