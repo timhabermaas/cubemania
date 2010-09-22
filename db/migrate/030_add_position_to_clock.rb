3f4cf394113e21c3d931e64b24b03a3301eab4c4
Tim Habermaas <tim.habermaas@gmail.com>
2010-09-22 10:59:38 +0200

    fixed another migration issue

--- db/migrate/030_add_position_to_clock.rb
+++ db/migrate/030_add_position_to_clock.rb
@@ -1,9 +1,14 @@
 class AddPositionToClock < ActiveRecord::Migration
+  class Clock < ActiveRecord::Base
+  end
+  class Average < Clock
+  end
+
   def self.up
     add_column :clocks, :position, :integer
     averages = Average.find :all
     for average in averages
-      index = 0;
+      index = 0
       average.singles.each {|s| s.update_attribute :position, index; index += 1 }
     end
   end
