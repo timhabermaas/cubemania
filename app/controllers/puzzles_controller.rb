89942ea70fd95b97887e435f65cb645234470e12
Tim Habermaas <tim.habermaas@gmail.com>
2010-10-23 14:25:52 +0200

    cleaned up puzzles code

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -1,18 +1,15 @@
 class PuzzlesController < ApplicationController
   permit :admin
-  
+
   def new
-    @kinds = Kind.all
     @puzzle = Puzzle.new
   end
-  
+
   def edit
-    @kinds = Kind.all
     @puzzle = Puzzle.find params[:id]
   end
-  
+
   def update
-    @kinds = Kind.all
     @puzzle = Puzzle.find params[:id]
     if @puzzle.update_attributes params[:puzzle]
       redirect_to puzzles_path
@@ -20,7 +17,7 @@ class PuzzlesController < ApplicationController
       render :edit
     end
   end
-  
+
   def create
     @puzzle = Puzzle.new params[:puzzle]
     if @puzzle.save
@@ -29,11 +26,11 @@ class PuzzlesController < ApplicationController
       render :new
     end
   end
-  
+
   def index
     @puzzles = Puzzle.order('kind_id, name').includes(:kind)
   end
-  
+
   def object
     @puzzle = Puzzle.find params[:id]
   end
