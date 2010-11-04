6e4221344f46dd2e7894196673319da94a7b8730
Tim Habermaas <tim.habermaas@gmail.com>
2010-11-04 19:40:00 +0100

    listing average of 12 at records page

--- config/routes.rb
+++ config/routes.rb
@@ -38,8 +38,8 @@ Cubemania::Application.routes.draw do
     #match 'competitions/:id/:date' => 'competitions#show', :as => 'competition_date'
     #match 'records/:type' => 'records#index', :as => 'records', :defaults => { :type => 'average' }, :type => /(single)|(average)/
   end
-  match 'puzzles/:puzzle_id/records/:type' => 'records#index', :as => 'puzzle_records', :defaults => { :type => 'average', :puzzle_id => DEFAULT_PUZZLE },
-          :type => /(single)|(average)/
+  match 'puzzles/:puzzle_id/records/:type' => 'records#index', :as => 'puzzle_records', :defaults => { :type => 'avg5', :puzzle_id => DEFAULT_PUZZLE },
+          :type => /(single)|(avg5)|(avg12)/
   match 'puzzles/:puzzle_id/times' => 'times#index', :as => 'puzzle_times', :defaults => { :puzzle_id => DEFAULT_PUZZLE }
   match 'puzzles/:puzzle_id/competitions' => 'competitions#index', :as => 'puzzle_competitions', :defaults => { :puzzle_id => DEFAULT_PUZZLE }
   match 'puzzles/:puzzle_id/competitions/:id/:date' => 'competitions#show', :as => 'puzzle_competition_date'
