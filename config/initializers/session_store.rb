3b2c1a1f9f52c54a3ca188605058695df98edff4
Tim Habermaas <tim.habermaas@gmail.com>
2011-04-05 00:42:47 +0200

    Changed session key to '_Cubemania_session' for consistency

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, {
-  :key => '_Cubemania_git_session',
+  :key => '_Cubemania_session',
   :expire_after => 14.days
 }
 
