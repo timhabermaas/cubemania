887664c4972a39d7e0919871f9715b35e3a7f142
Tim Habermaas <tim.habermaas@gmail.com>
2011-06-10 00:56:05 +0200

    don't create a chart if there's no #chart element on the page

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -1,52 +1,54 @@
 var chart;
 
 $(document).ready(function() {
-  var singles = $("#singles li #time").map(function() {
-    return parseInt($(this).data("time"));
-  }).get().reverse();
+  if ($("#chart").length > 0) {
+    var singles = $("#singles li #time").map(function() {
+      return parseInt($(this).data("time"));
+    }).get().reverse();
 
-  chart = new Highcharts.Chart({
-           chart: {
-             renderTo: 'chart'
-           },
-           title: {
-             text: 'Cubing Progress'
-           },
-           xAxis: {
-             title: {
-               text: 'Attempts'
+    chart = new Highcharts.Chart({
+             chart: {
+               renderTo: 'chart'
              },
-             allowDecimals: false,
-             labels: {
-               formatter: function() {
-                 return this.value + 1;
+             title: {
+               text: 'Cubing Progress'
+             },
+             xAxis: {
+               title: {
+                 text: 'Attempts'
+               },
+               allowDecimals: false,
+               labels: {
+                 formatter: function() {
+                   return this.value + 1;
+                 }
                }
-             }
-           },
-           yAxis: {
-              title: {
-                 text: 'Time'
-              },
-              labels: {
-                formatter: function() {
-                  return formatTime(this.value);
+             },
+             yAxis: {
+                title: {
+                   text: 'Time'
+                },
+                labels: {
+                  formatter: function() {
+                    return formatTime(this.value);
+                  }
                 }
-              }
-           },
-           tooltip: {
-             formatter: function() {
-               return "hey :)";
-             }
-           },
-           plotOptions: {
-                    line: {
-                       enableMouseTracking: true,
-                       allowPointSelect: true
-                    }
-                 },
-           series: [{
-              name: $("#singles").attr("data-name"),
-              data: singles
-           }]
-        });
+             },
+             tooltip: {
+               formatter: function() {
+                 return "hey :)";
+               }
+             },
+             plotOptions: {
+                      line: {
+                         enableMouseTracking: true,
+                         allowPointSelect: true
+                      }
+                   },
+             series: [{
+                name: $("#singles").attr("data-name"),
+                data: singles
+             }]
+          });
+  }
 });
