1065cef9167e61c580f52fd834fbd627c96db7a4
Tim Habermaas <tim.habermaas@gmail.com>
2011-06-18 00:10:00 +0200

    added restart task for delayed_job to deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,12 +2,14 @@ $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory
 $:.unshift(File.join(File.dirname(__FILE__), 'deploy'))
 require "capistrano_database_yml"
 require "rvm/capistrano"
+require "delayed/recipes"
 
 set :rvm_ruby_string, '1.9.2'
 # set :rvm_type, :user if you don't want to use systemwide install, recommend
 
 set :application, "cubemania_beta"
 set :repository,  "git@timhabermaas.beanstalkapp.com:/cubemania.git"
+set :rails_env, "production"
 
 set :scm, :git
 set :branch, "rails31"
@@ -29,13 +31,20 @@ after "deploy:bundle_gems", "deploy:db:setup"
 after "deploy:db:setup", "deploy:symlink_shared"
 after "deploy:symlink_shared", "deploy:precompile_assets"
 
+# delayed_job
+before "deploy:restart", "delayed_job:stop"
+after  "deploy:restart", "delayed_job:start"
+
+after "deploy:stop",  "delayed_job:stop"
+after "deploy:start", "delayed_job:start"
+
 namespace :deploy do
   task :bundle_gems do
     run "cd #{release_path} && bundle install"
   end
   desc "Compile assets"
   task :precompile_assets do
-    run "cd #{release_path}; RAILS_ENV=production bundle exec rake assets:precompile"
+    run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake assets:precompile"
   end
   task :start do ; end
   task :stop do ; end
