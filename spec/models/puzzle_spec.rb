35b6dea05b620045a46d47e308e0733bbb9c9bfa
Tim Habermaas <tim.habermaas@gmail.com>
2012-01-12 00:41:51 +0100

    fix puzzle spec

--- spec/models/puzzle_spec.rb
+++ spec/models/puzzle_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe Puzzle do
   describe "scrambles" do
     it "delivers 25 move 3x3x3 scramble" do
-      Puzzle.new(:name => "3x3x3", :attempt_count => 25).scramble.split(" ").should have(25).elements
+      Puzzle.new(:name => "3x3x3", :scramble_length => 25).scramble.split(" ").should have(25).elements
     end
   end
 end
