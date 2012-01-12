75d252ad4f20874da380fde696b113968a264699
Tim Habermaas <tim.habermaas@gmail.com>
2012-01-12 19:43:05 +0100

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
-k = Kind.create!(:name => 'speed')
-Kind.create!(:name => 'blindfolded')
-Kind.create!(:name => 'one-handed')
+speed = Kind.create! :name => 'speed', :css_class => 'speed', :css_position => 0
+Kind.create! :name => 'blindfolded', :css_class => 'bld', :css_position => 1
+Kind.create! :name => 'one-handed', :css_class => 'oh', :css_position => 2
 
-Puzzle.create!(:name => '3x3x3', :kind => k, :scramble_length => 25, :attempt_count => 5, :average_format => 'average')
+Puzzle.create! :name => '3x3x3',
+               :kind => speed,
+               :scramble_length => 25,
+               :attempt_count => 5,
+               :average_format => 'average',
+               :css_class => "three", :css_position => 1
