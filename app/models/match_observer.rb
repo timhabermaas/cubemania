1c8e001dcd95cc8291e9d874cc8c707d3a7b5d57
Tim Habermaas <tim.habermaas@gmail.com>
2012-01-21 23:17:36 +0100

    remove match observer

--- app/models/match_observer.rb
+++
@@ -1,7 +0,0 @@
-class MatchObserver < ActiveRecord::Observer
-  def after_save(match)
-    if match.challenged? and match.opponent.wants_emails?
-      UserMailer.deliver_match_mail(match.opponent, match)
-    end
-  end
-end
