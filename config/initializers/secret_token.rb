11ddc9621f08ac9744d48e0c1bb1479610666963
Tim Habermaas <tim.habermaas@gmail.com>
2012-01-21 04:38:19 +0100

    use environment variable for secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = '51e5bc788df528014ac1f5fc8d0d8617a204e0579a904a3832f29019f353ed4a9c77371b6de94a7cfa9c5655a5a6e5e1daaf65224b8efda73b7b12a4c205b953'
+Rails.application.config.secret_token = ENV['SECRET_TOKEN'] || '51e5bc788df528014ac1f5fc8d0d8617a204e0579a904a3832f29019f353ed4a9c77371b6de94a7cfa9c5655a5a6e5e1daaf65224b8efda73b7b12a4c205b953'
