caabdcbbd76783ca03b5bb19645d74fcde0f5e1b
Tim Habermaas <tim.habermaas@gmail.com>
2012-01-22 20:35:13 +0100

    delete old initializer needed for record calculation in C

--- config/initializers/set_db_environment_variables.rb
+++
@@ -1,3 +0,0 @@
-ENV['CUBEMANIA_DATABASE'] = Cubemania::Application.config.database_configuration[Rails.env]["database"]
-ENV['CUBEMANIA_USERNAME'] = Cubemania::Application.config.database_configuration[Rails.env]["username"]
-ENV['CUBEMANIA_PASSWORD'] = Cubemania::Application.config.database_configuration[Rails.env]["password"]
