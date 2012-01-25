a3efaba645269eca7eab776ede2a52f048e6cacc
Tim Habermaas <tim.habermaas@gmail.com>
2012-01-25 02:35:57 +0100

    allow ordinary users to compete in competitions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -13,6 +13,7 @@ class Ability
       end
       can :create, Comment
       can :create, Competition
+      can :add_average, Competition
       can :update, Competition do |competition|
         competition.user == user
       end
