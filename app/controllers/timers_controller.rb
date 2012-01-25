b3c6553c117807473b27e24e8ec39141884da35e
Tim Habermaas <tim.habermaas@gmail.com>
2012-01-25 20:58:03 +0100

    update records when user dnfs/destroys or plus2 a single

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -32,6 +32,7 @@ class TimersController < ApplicationController
   def dnf
     @single = current_user.singles.find params[:id]
     @single.toggle_dnf!
+    UpdateRecords.for(current_user, @puzzle)
     respond_to do |format|
       format.html { redirect_to puzzle_timers_path(@puzzle) }
       format.js { render :penalty }
@@ -41,6 +42,7 @@ class TimersController < ApplicationController
   def plus2
     @single = current_user.singles.find params[:id]
     @single.toggle_plus2!
+    UpdateRecords.for(current_user, @puzzle)
     respond_to do |format|
       format.html { redirect_to puzzle_timers_path(@puzzle) }
       format.js { render :penalty }
@@ -50,6 +52,7 @@ class TimersController < ApplicationController
   def destroy
     @single = current_user.singles.find params[:id]
     @single.destroy
+    UpdateRecords.for(current_user, @puzzle)
     respond_to do |format|
       format.html { redirect_to puzzle_timers_path(@puzzle) }
       format.js
