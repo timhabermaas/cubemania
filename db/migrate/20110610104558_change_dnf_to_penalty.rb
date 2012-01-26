d90dd4054ca23ac52cd8c6263338fa074a8875fb
Tim Habermaas <tim.habermaas@gmail.com>
2012-01-26 21:13:36 +0100

    fix another migration... oh, boy...

--- db/migrate/20110610104558_change_dnf_to_penalty.rb
+++ db/migrate/20110610104558_change_dnf_to_penalty.rb
@@ -3,7 +3,9 @@ class ChangeDnfToPenalty < ActiveRecord::Migration
     add_column :singles, :penalty, :string, :limit => 8
     Single.reset_column_information
     say_with_time "set penalty to 'dnf' for all dnfs" do
-      Single.find_by_sql("UPDATE singles s SET penalty='dnf' WHERE s.dnf=1")
+      Single.find_each do |s|
+        s.update_attribute(:penalty, "dnf") if s.dnf == true
+      end
     end
     remove_column :singles, :dnf
   end
