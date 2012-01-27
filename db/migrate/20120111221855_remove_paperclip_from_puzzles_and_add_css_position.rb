98a771fc4b1507c237076d71a5c3b6898435efd5
Tim Habermaas <tim.habermaas@gmail.com>
2012-01-27 01:09:14 +0100

    clean up migrations

--- db/migrate/20120111221855_remove_paperclip_from_puzzles_and_add_css_position.rb
+++ db/migrate/20120111221855_remove_paperclip_from_puzzles_and_add_css_position.rb
@@ -1,15 +1,13 @@
-class RemovePaperclipFromPuzzlesAndAddCssClassAndPosition < ActiveRecord::Migration
+class RemovePaperclipFromPuzzlesAndAddCssPosition < ActiveRecord::Migration
   def up
     remove_column :puzzles, :image_file_name
     remove_column :puzzles, :image_content_type
     remove_column :puzzles, :image_file_size
     remove_column :puzzles, :image_updated_at
-    add_column :puzzles, :css_class, :string, :null => false, :default => "dummy"
     add_column :puzzles, :css_position, :integer, :null => false, :default => 0
   end
 
   def down
-    remove_column :puzzles, :css_class
     remove_column :puzzles, :css_position
     add_column :puzzles, :image_file_name, :string
     add_column :puzzles, :image_content_type, :string
--- db/migrate/20120111230201_remove_paperclip_from_kinds_and_add_css_position.rb
+++ db/migrate/20120111230201_remove_paperclip_from_kinds_and_add_css_position.rb
@@ -1,15 +1,13 @@
-class RemovePaperclipFromKindsAndAddCssClassAndPosition < ActiveRecord::Migration
+class RemovePaperclipFromKindsAndAddCssPosition < ActiveRecord::Migration
   def up
     remove_column :kinds, :image_file_name
     remove_column :kinds, :image_content_type
     remove_column :kinds, :image_file_size
     remove_column :kinds, :image_updated_at
-    add_column :kinds, :css_class, :string, :null => false, :default => "dummy"
     add_column :kinds, :css_position, :integer, :null => false, :default => 0
   end
 
   def down
-    remove_column :kinds, :css_class
     remove_column :kinds, :css_position
     add_column :kinds, :image_file_name, :string
     add_column :kinds, :image_content_type, :string
