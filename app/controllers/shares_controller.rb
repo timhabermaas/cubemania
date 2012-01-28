a03fa5e3ac81b88cf87a3ed4d76a793c938414cf
Tim Habermaas <tim.habermaas@gmail.com>
2012-01-28 03:52:02 +0100

    redirect to /auth/facebook when user is not authorized

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -3,11 +3,20 @@ class SharesController < ApplicationController
 
   def create
     @record = current_user.records.find params[:record_id]
-    me = FbGraph::User.me(current_user.authorizations.find_by_provider("facebook").token)
-    me.feed! :message => "test",
-             :name => RecordPresenter.new(@record).record_type,
-             :picture => @record.puzzle.combined_url,
-             :link => puzzle_record_url(@record.puzzle, @record)
-    redirect_to root_path
+    token = current_user.authorizations.find_by_provider("facebook").try(:token)
+    if token
+      begin
+        me = FbGraph::User.me(token)
+        me.feed! :message => "test",
+                 :name => RecordPresenter.new(@record).record_type,
+                 :picture => @record.puzzle.combined_url,
+                 :link => puzzle_record_url(@record.puzzle, @record)
+        redirect_to root_path, :notice => "Successfully shared."
+      rescue FbGraph::InvalidToken
+        redirect_to "/auth/facebook"
+      end
+    else
+      redirect_to "/auth/facebook"
+    end
   end
 end
--- spec/controllers/shares_controller_spec.rb
+++ spec/controllers/shares_controller_spec.rb
@@ -2,13 +2,16 @@ require "spec_helper"
 
 describe SharesController do
   describe "#create" do
-    context "provider = facebook" do
-      let(:user) { create :user }
-      let(:record) { create :record, :user => user, :amount => 5 }
+    let(:user) { create :user }
+    let(:record) { create :record, :user => user, :amount => 5 }
 
+    before :each do
+      controller.stub!(:current_user) { user }
+    end
+
+    context "user is already authorized" do
       before :each do
         user.authorizations << Authorization.new(:provider => "facebook", :token => "foo_token", :uid => "dasd")
-        controller.stub!(:current_user) { user }
       end
 
       it "posts on fb wall" do
@@ -16,6 +19,25 @@ describe SharesController do
         FbGraph::User.should_receive(:me).with("foo_token").and_return(me)
         me.should_receive(:feed!).with(hash_including(:link => "http://test.host/puzzles/#{record.puzzle.slug}/records/#{record.id}", :name => "Average of 5"))
         post :create, :record_id => record.id
+      end
+    end
+
+    context "user is not authorized" do
+      it "redirects to /auth/facebook" do
+        FbGraph::User.should_not_receive(:me)
+        post :create, :record_id => record.id
+        response.should redirect_to("/auth/facebook")
+      end
+    end
+
+    context "user is authorized, but access token is no longer valid" do
+      before :each do
+        user.authorizations << Authorization.new(:provider => "facebook", :token => "invalid_token", :uid => "dasd")
+      end
+
+      it "redirects to /auth/facebook" do
+        post :create, :record_id => record.id
+        response.should redirect_to("/auth/facebook")
       end
     end
   end
