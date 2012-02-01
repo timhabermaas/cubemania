c23a479f06dbab4c1ac187441604084d1e6b6e9f
Tim Habermaas <tim.habermaas@gmail.com>
2012-02-01 03:04:42 +0100

    remove unnecessary facebook permissions

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"]
-  provider :facebook, ENV["FACEBOOK_APP_KEY"], ENV["FACEBOOK_APP_SECRET"], :scope => "publish_stream,publish_actions,user_likes"
+  provider :facebook, ENV["FACEBOOK_APP_KEY"], ENV["FACEBOOK_APP_SECRET"], :scope => "publish_stream"
   provider :developer unless Rails.env.production?
 end
 
