512d00c6a4a7c55a28d9ffc2dfaef8d40d0d08f8
Tim Habermaas <tim.habermaas@gmail.com>
2012-04-23 23:39:18 +0200

    don't set time zone in Rails to get UTC dates from json responses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,17 +18,9 @@ class ApplicationController < ActionController::Base
     redirect_to root_path
   end
 
-  before_filter :set_time_zone, :store_return_to
+  before_filter :store_return_to
 
 private
-  def set_time_zone
-    if logged_in?
-      Time.zone = current_user.time_zone
-    elsif not cookies[:tz_offset].blank?
-      Time.zone = ActiveSupport::TimeZone[-cookies[:tz_offset].to_i.minutes]
-    end
-  end
-
   def store_return_to
     store_location params[:return_to] unless params[:return_to].nil?
   end
