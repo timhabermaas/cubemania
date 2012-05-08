35fd0fa50be3161751f3e16c1611641b0ac95b6f
Tim Habermaas <tim.habermaas@gmail.com>
2012-05-09 01:24:29 +0200

    moar cleanup

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,6 @@ class HomesController < ApplicationController
   skip_load_and_authorize_resource
 
   def show
-    @announcement = Post.order('created_at desc').includes(:user).first
+    @announcement = Post.order('created_at desc').first
   end
 end
