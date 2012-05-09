97695489241b2c0f2b2ca99f954938821aa9c48d
Tim Habermaas <tim.habermaas@gmail.com>
2012-05-09 03:40:01 +0200

    clean up application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   include Authentication
 
-  after_filter :flash_to_headers
-
   login :except => [:index, :show]
 
   load_and_authorize_resource
@@ -10,11 +8,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   rescue_from ActiveRecord::RecordNotFound do |exception|
-    render 'errors/not_found'
+    render "errors/not_found"
   end
 
   rescue_from CanCan::AccessDenied do |exception|
-    flash[:notice] = 'You do not have the necessary permissions'
+    flash[:notice] = "You do not have the necessary permissions!"
     redirect_to root_path
   end
 
@@ -23,16 +21,5 @@ class ApplicationController < ActionController::Base
 private
   def store_return_to
     store_location params[:return_to] unless params[:return_to].nil?
-  end
-
-  def flash_to_headers
-    return unless request.xhr?
-    [:notice, :alert].each do |type|
-      if flash[type].present?
-        response.headers['X-Message'] = flash[type]
-        response.headers['X-Type'] = type.to_s
-      end
-    end
-    flash.discard
   end
 end
