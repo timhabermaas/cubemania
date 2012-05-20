96ff624205d431fc0bcc3aff7c7df183f50900d4
Tim Habermaas <tim.habermaas@gmail.com>
2012-05-20 15:36:29 +0200

    add puzzle name to facebook message

--- app/presenters/record_presenter.rb
+++ app/presenters/record_presenter.rb
@@ -10,14 +10,19 @@ class RecordPresenter
     amount = @record.amount
     case amount
     when 1
-      "Single"
+      "single"
     else
-      "Average of #{amount}"
+      "average of #{amount}"
     end
   end
 
   def flash_message
     "You have a new #{record_type.downcase} record: <strong>#{@record.human_time}</strong>!"
+  end
+
+  def full_puzzle_name
+    kind = @record.puzzle.kind.name == "speed" ? "" : @record.puzzle.kind.short_name.upcase
+    [@record.puzzle.name, kind].join " "
   end
 
   def singles_as_text
