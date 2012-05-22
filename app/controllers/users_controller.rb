b13f128e9c2cdf433b039552a5b247e568d8ff76
Tim Habermaas <tim.habermaas@gmail.com>
2012-05-22 23:40:20 +0200

    display more users on users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   #protect [:role, :sponsor, :ignored], :but => :admin, :only => [:create, :update]
 
   def index
-    @users = User.order('singles_count desc').paginate(:page => params[:page], :per_page => 100)
+    @users = User.order('singles_count desc').paginate(:page => params[:page], :per_page => 200)
     @users = @users.where('lower(name) LIKE ?', "%#{params[:q].downcase}%") if params[:q]
 
     respond_to do |format|
