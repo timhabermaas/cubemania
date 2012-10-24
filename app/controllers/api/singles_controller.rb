51cb7b5453dea5f6b33031eeeeb8a9141a9d9c2b
Tim Habermaas <tim.habermaas@gmail.com>
2012-10-24 14:54:31 +0200

    fetch users in singles controller; fix forgotton @

--- app/controllers/api/singles_controller.rb
+++ app/controllers/api/singles_controller.rb
@@ -1,19 +1,18 @@
 module Api
   class SinglesController < ApiController
     respond_to :json
+    before_filter :fetch_user, :only => [:index, :grouped]
     before_filter :fetch_puzzle
 
     def index
-      user = User.find params[:user_id]
-      @singles = user.singles.for(@puzzle).limit(params[:limit] || 150)
+      @singles = @user.singles.for(@puzzle).limit(params[:limit] || 150)
       respond_to do |format|
         format.json
       end
     end
 
     def grouped
-      user = User.find params[:user_id]
-      @singles = user.singles.where(:puzzle_id => @puzzle.id).grouped(by: params[:by]).order("created_at desc")
+      @singles = @user.singles.where(:puzzle_id => @puzzle.id).grouped(by: params[:by]).order("created_at desc")
 
       respond_to do |format|
         format.json
@@ -23,7 +22,7 @@ module Api
     def create
       single = current_user.singles.build(params[:single].merge(:puzzle_id => @puzzle.id))
       if single.save
-        if UpdateRecentRecords.for(current_user, puzzle)
+        if UpdateRecentRecords.for(current_user, @puzzle)
           response.headers["X-NewRecord"] = "true"
         end
       end
@@ -56,6 +55,10 @@ module Api
     def enqueue_record_job(user, puzzle)
       job = RecordCalculationJob.new(user.id, puzzle.id)
       Delayed::Job.enqueue job unless job.exists?
+    end
+
+    def fetch_user
+      @user = User.find params[:user_id]
     end
 
     def fetch_puzzle
