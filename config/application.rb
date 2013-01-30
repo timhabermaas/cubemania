c65fd7b534eee39141315bfcbf24c2187fda31b7
Tim Habermaas <tim.habermaas@gmail.com>
2013-01-30 22:11:44 +0100

    move ability definitions to app/abilities

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Cubemania
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/presenters)
+    config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/presenters #{config.root}/app/abilities)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
