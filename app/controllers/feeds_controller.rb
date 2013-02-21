bdc7c6b4daf5b4b6a07cc0cef5e06c1c703425fb
Tim Habermaas <tim.habermaas@gmail.com>
2013-02-21 17:30:00 +0100

    style activity feed

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,5 +1,5 @@
 class FeedsController < ApplicationController
   def show
-    @feed = Feed.new current_user
+    @activities = Feed.new(current_user).activities.group_by { |a| a.updated_at.beginning_of_day }
   end
 end
