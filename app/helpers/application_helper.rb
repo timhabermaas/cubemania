95597017cc9d746385f7b5264672316c26549c37
Tim Habermaas <tim.habermaas@gmail.com>
2013-02-28 01:34:35 +0100

    fix crashes in comments when user is nil

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def avatar_url(user, size)
-    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    gravatar_id = Digest::MD5.hexdigest(user ? user.email.downcase : "")
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
   end
 
