1856f46d7c90006b5839e3aea1388fd969eec0df
Tim Habermaas <tim.habermaas@gmail.com>
2014-08-20 22:37:17 +0200

    Bump asset version.

--- config/application.rb
+++ config/application.rb
@@ -57,6 +57,6 @@ module Cubemania
     config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.10'
+    config.assets.version = '1.11'
   end
 end
