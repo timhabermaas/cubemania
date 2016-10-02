5231b34d4df3daaa1998466bd3caa0938055af0f
Tim Habermaas <tim.habermaas@gmail.com>
2016-10-02 13:00:51 +0200

    Store sessions in database

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, {
+Rails.application.config.session_store :active_record_store, {
   :key => '_Cubemania_session',
   :expire_after => 14.days
 }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160909103117) do
+ActiveRecord::Schema.define(:version => 20161002105813) do
 
   create_table "activities", :force => true do |t|
     t.integer  "trackable_id",                  :null => false
@@ -116,6 +116,16 @@ ActiveRecord::Schema.define(:version => 20160909103117) do
   end
 
   add_index "records_singles", ["record_id", "single_id"], :name => "index_records_singles_on_record_id_and_single_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "singles", :force => true do |t|
     t.integer  "time",                       :null => false
