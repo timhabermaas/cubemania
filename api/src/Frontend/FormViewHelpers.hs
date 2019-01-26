ffa80338ddbeb8df95991116207d8b23d9ec6884
Tim Habermaas <tim.habermaas@gmail.com>
2019-01-26 22:29:24 +0100

    [WIP] Fix label#for attributes

--- api/src/Frontend/FormViewHelpers.hs
+++ api/src/Frontend/FormViewHelpers.hs
@@ -24,10 +24,11 @@ checkbox :: T.Text -> T.Text -> View Html -> Html
 checkbox ref labelName view =
     let ref' = toValue $ absoluteRef ref view
         checkedAttribute = if fieldInputBool ref view then A.checked "" else mempty
+        cssId = ref'
     in
         li ! class_ "boolean input optional" $ do
-            H.label ! class_ "" ! for "user_wants_emails" $ do
-                input ! A.id ref' ! name ref' ! type_ "checkbox" ! checkedAttribute
+            H.label ! class_ "" ! for cssId $ do
+                input ! A.id cssId ! name ref' ! type_ "checkbox" ! checkedAttribute
                 toMarkup labelName
 
 -- TODO: Can this be replaced with childErrors from Text.Digestive.View?
@@ -38,14 +39,16 @@ textFieldWithDifferentErrors' fieldName errorNames labelName form' isRequired ty
         errorList = mapM_ (p ! class_ "inline-errors") (join $ fmap (\en -> errors en form') errorNames)
         labelAnnotation Required = abbr ! A.title "required" $ "*"
         labelAnnotation Optional = return ()
+        cssId = H.toValue $ absoluteRef fieldName form'
     in
         li ! class_ (toValue $ "text input" <> errorClass <> " " <> fieldName) $ do
             H.label ! A.class_ "label"
-                    ! A.for (H.toValue (absoluteRef fieldName form')) $ do
+                    ! A.for cssId $ do
                 toHtml labelName
                 labelAnnotation isRequired
             input ! name (toValue $ absoluteRef fieldName form')
                   ! type_ (toValue type')
+                  ! A.id cssId
                   ! value (toValue $ fieldInputText fieldName form')
             errorList
 
@@ -66,7 +69,7 @@ passwordFieldWithDifferentErrors fieldName errorNames labelName form' = textFiel
 timeZoneField :: T.Text -> T.Text -> View Html -> Html
 timeZoneField fieldName labelName form' =
     li ! class_ "time_zone input optional" ! A.id "user_time_zone_input" $ do
-        H.label ! class_ " label" ! for ref $ toMarkup labelName
+        H.label ! class_ "label" ! for ref $ toMarkup labelName
         select ! A.id ref ! name ref $
             mapM_ entry (fieldInputChoice fieldName form')
   where
@@ -80,13 +83,15 @@ textareaWithErrors fieldName labelName form' isRequired =
         errorList = mapM_ (p ! class_ "inline-errors") (errors fieldName form')
         labelAnnotation Required = abbr ! A.title "required" $ "*"
         labelAnnotation Optional = return ()
+        cssId = H.toValue $ absoluteRef fieldName form'
     in
         li ! class_ ("text input" <> errorClass) $ do
             H.label ! A.class_ "label"
-                    ! A.for (H.toValue (absoluteRef fieldName form')) $ do
+                    ! A.for cssId $ do
                 toHtml labelName
                 labelAnnotation isRequired
             textarea ! name (toValue $ absoluteRef fieldName form')
+                     ! A.id cssId
                      ! rows "4" $
                 toHtml $ fieldInputText fieldName form'
             errorList
