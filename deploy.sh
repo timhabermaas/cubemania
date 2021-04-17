7874ec7c31d77086dc5de13a628ee902f05cb942
Tim Habermaas <tim.habermaas@gmail.com>
2021-04-17 12:26:50 +0200

    Simplify deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,5 @@
 set -e
 
 docker-compose run web bundle exec rake assets:precompile
-docker build -f nginx/Dockerfile -t timhabermaas/cubemania_nginx .
-docker build -t timhabermaas/cubemania_rails .
-docker build -f backup/Dockerfile -t timhabermaas/cubemania_backup backup
-docker build -f records/Dockerfile -t timhabermaas/cubemania_records records
-docker push timhabermaas/cubemania_nginx
-docker push timhabermaas/cubemania_rails
-docker push timhabermaas/cubemania_backup
-docker push timhabermaas/cubemania_records
+docker-compose build
+docker-compose push
