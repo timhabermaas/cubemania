557c0bb6abc6ed8bf13edec669c07fd695044895
Tim Habermaas <tim.habermaas@gmail.com>
2021-05-02 17:01:05 +0200

    Use separate docker config for prod to not expose all ports/volume mappings by default

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 set -e
 
-docker-compose build web
-docker-compose run web bundle exec rake assets:precompile
-docker-compose build
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml build web
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml run web bundle exec rake assets:precompile
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml build
--- restart.sh
+++ restart.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-docker-compose down
-docker-compose up -d nginx
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml down
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d nginx
