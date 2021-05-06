4247a31fb3a25167cccd2fe803258741106f77fd
Tim Habermaas <tim.habermaas@gmail.com>
2021-05-06 23:33:10 +0200

    Make bucket name configurable in backup script

--- backup/backup.sh
+++ backup/backup.sh
@@ -6,5 +6,5 @@ file=/data/cubemania-database-$(date +%Y-%m-%d_%H-%M-%S).dump.gz
 
 # `-Fc`: custom format suitable for pg_restore.
 pg_dump -Fc -h db -U postgres cubemania_production | gzip > $file
-aws s3 cp $file s3://backup.cubemania.org/
+aws s3 cp $file s3://${AWS_BUCKET_NAME}/
 rm $file
