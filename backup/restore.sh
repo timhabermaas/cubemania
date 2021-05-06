9225174143cdafa881b065468285e13e1d7b24c1
Tim Habermaas <tim.habermaas@gmail.com>
2021-05-06 23:04:23 +0200

    Run backup container when starting nginx

--- backup/restore.sh
+++ backup/restore.sh
@@ -6,6 +6,6 @@ filename=$1
 
 target=/data/current.dump.gz
 
-aws s3 cp s3://backup.cubemania.org/$filename $target
+aws s3 cp s3://$AWS_BUCKET_NAME/$filename $target
 gzip -d $target
 pg_restore --verbose --clean --no-acl --no-owner -h db -U postgres -d cubemania_production /data/current.dump
