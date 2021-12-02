95d1e572e10a6d406f9a79b233bb6b7b21ef79e5
Tim Habermaas <tim.habermaas@gmail.com>
2021-12-03 00:58:07 +0100

    Update postgres version to 13.5

---
+++ config/initializers/postgresql_adapter_monkeypatch.rb
@@ -0,0 +1,13 @@
+# monkey patch required to make ActiveRecord 3.x work with PostgreSQL >= 12.x
+# https://stackoverflow.com/questions/58763542/pginvalidparametervalue-error-invalid-value-for-parameter-client-min-messag
+
+require 'active_record/connection_adapters/postgresql_adapter'
+
+class ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
+  def set_standard_conforming_strings
+    old, self.client_min_messages = client_min_messages, 'warning'
+    execute('SET standard_conforming_strings = on', 'SCHEMA') rescue nil
+  ensure
+    self.client_min_messages = old
+  end
+end
