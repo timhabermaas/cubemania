b06b727176105895e2b088afd82ac0c0d2a89942
Tim Habermaas <tim.habermaas@gmail.com>
2021-12-03 01:35:02 +0100

    Set pg password for pg_dump/pg_restore

--- backup/backup.sh
+++ backup/backup.sh
@@ -5,6 +5,6 @@ set -e
 file=/data/cubemania-database-$(date +%Y-%m-%d_%H-%M-%S).dump.gz
 
 # `-Fc`: custom format suitable for pg_restore.
-pg_dump -Fc -h db -U postgres cubemania_production | gzip > $file
+PGPASSWORD=${POSTGRES_PASSWORD} pg_dump -Fc -h db -U postgres cubemania_production | gzip > $file
 aws s3 cp $file s3://${AWS_BUCKET_NAME}/
 rm $file
--- backup/restore.sh
+++ backup/restore.sh
@@ -8,4 +8,4 @@ target=/data/current.dump.gz
 
 aws s3 cp s3://$AWS_BUCKET_NAME/$filename $target
 gzip -d $target
-pg_restore --verbose --clean --no-acl --no-owner -h db -U postgres -d cubemania_production /data/current.dump
+PGPASSWORD=${POSTGRES_PASSWORD} pg_restore --verbose --clean --no-acl --no-owner -h db -U postgres -d cubemania_production /data/current.dump
