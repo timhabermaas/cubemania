e6a2f960a30a36dc03908f0db8bc5e14f1e8a258
Tim Habermaas <tim.habermaas@gmail.com>
2022-10-08 18:06:12 +0200

    Simplify deployment by using remote images

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-set -e
-
-env DOCKER_BUILDKIT=1 docker-compose -f docker-compose.yml -f docker-compose.prod.yml build web
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml run --rm web bundle exec rake assets:precompile
-env DOCKER_BUILDKIT=1 docker-compose -f docker-compose.yml -f docker-compose.prod.yml build
--- restart.sh
+++ restart.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml down
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d nginx
